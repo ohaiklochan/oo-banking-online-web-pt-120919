0220c606fa67f91a181139f24c02af1b039536ad
Chloe Foronda <caforonda103@yahoo.com>
2020-01-31 00:44:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,8 @@ class Transfer
   def execute_transaction
     if @sender.balance < @amount
       @status = "rejected"
-      
+      return "Transaction rejected. Please check your account balance."
+    else 
       
   
 end
