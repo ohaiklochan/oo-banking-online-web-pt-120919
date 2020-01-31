f0b29eccf928219ddf0c310bd21fe370fe3369c7
Chloe Foronda <caforonda103@yahoo.com>
2020-01-31 00:31:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,12 @@
 class Transfer
   
-  attr_accessor :transfer, :name, :receiver, :status, :amount
+  attr_accessor :transfer, :sender, :receiver, :status, :amount
   
   def initialize(transfer)
-    
+    @sender = sender
+    @receiver = receiver
+    @status = "open"
+    @amount = 1000
   end
   
   
